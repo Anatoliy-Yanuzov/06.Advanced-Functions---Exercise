a8a131dabdf0b4ece494b240fe1e16a8e9083e5b
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-03-13 01:04:13 +0200

    solution

--- 01. Sort Array.js
+++ 01. Sort Array.js
@@ -9,5 +9,5 @@ function solve(arr, sortType) {
     // Solved with a ternary operator
     // return sortType === 'asc' ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a);
 }
-
+console.log(solve([14, 7, 17, 6, 8], 'asc'));
 
