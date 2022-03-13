1ef0c2b279369462c2d4be0fa7b8e07bd1550855
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-03-14 00:47:47 +0200

    solution

---
+++ 05. Functional Sum.js
@@ -0,0 +1,11 @@
+function solve(num) {
+    let temp = 0
+
+    function recursive(x) {
+        temp += x
+
+        return recursive
+    }
+    recursive.toString = () => temp
+    return recursive(num)
+}
